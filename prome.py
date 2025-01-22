20b8f8f39c0d10539afaac0b4dd15b613ad95480
Federico Tabbò <federico@tabbo.it>
2025-01-22 18:28:09 +0100

    Export all values to subnames

--- prome.py
+++ prome.py
@@ -21,23 +21,39 @@ prom_zones = {}
 prom_programs = {}
 
 for zone in z.root:
-    if zone.status == ZoneStatusEnum.UNKNOWN:
+    if zone.status == ZoneStatusEnum.UNKNOWN or not zone.allocated:
         continue
-    thisgauge = Gauge(name=PREFIX+"zone_"+zone.description
-        .replace(" ", "_")
-        .replace(".", "_")
-        .replace("-", "_")
-        .lower(), documentation="Zone")
-    if zone.allocated:
-        prom_zones[zone.description] = thisgauge
+
+    clean_name = PREFIX+"zone_"+zone.description \
+        .replace(" ", "_") \
+        .replace(".", "_") \
+        .replace("-", "_") \
+        .lower()
+
+    thisgauge = Gauge(name=clean_name+"_inFail", documentation="Zone Fail")
+    prom_zones[zone.description+"_inFail"] = thisgauge
+
+    thisgauge = Gauge(name=clean_name+"_inLowBattery",
+                      documentation="Zone Low Battery")
+    prom_zones[zone.description+"_inLowBattery"] = thisgauge
+
+    thisgauge = Gauge(name=clean_name+"_inSupervision",
+                      documentation="Zone is in Supervision mode")
+    prom_zones[zone.description+"_inSupervision"] = thisgauge
+
+    thisgauge = Gauge(name=clean_name+"_open", documentation="Zone is open")
+    prom_zones[zone.description+"_open"] = thisgauge
 
 
 for programstatus, programdata in zip(p.root, centrale.tp.status.programs):
     if len(programdata.zones) == 0:
         continue
 
-    clean_name = PREFIX+programdata.description.replace(
-        "-", "_").replace(" ", "_").lower()
+    clean_name = PREFIX+"program_"+programdata.description \
+        .replace(" ", "_") \
+        .replace(".", "_") \
+        .replace("-", "_") \
+        .lower()
 
     thisprogram = Gauge(name=clean_name+"_status",
                         documentation="Program Status")
@@ -65,9 +81,13 @@ if __name__ == '__main__':
         time.sleep(10)
         z = s.get_zones()
         for zone in z.root:
-            if zone.status == ZoneStatusEnum.UNKNOWN:
+            if zone.status == ZoneStatusEnum.UNKNOWN or not zone.allocated:
                 continue
-            prom_zones[zone.description].set(zone.open)
+            prom_zones[zone.description+"_inFail"].set(zone.inFail)
+            prom_zones[zone.description+"_inLowBattery"].set(zone.inLowBattery)
+            prom_zones[zone.description +
+                       "_inSupervision"].set(zone.inSupervision)
+            prom_zones[zone.description+"_open"].set(zone.open)
 
         p = s.get_programs()
         for programstatus, programdata in zip(p.root, centrale.tp.status.programs):
@@ -84,4 +104,4 @@ if __name__ == '__main__':
                           "_memAlarm"].set(programstatus.memAlarm)
             prom_programs[programdata.description +
                           "_prealarm"].set(programstatus.prealarm)
-        print("Done!")
+        print(dt.now())
